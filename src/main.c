f90d10c2bfec6da4364c0f3c0302be909974e884
Marc Sidorenko <marc.sidorenko@epita.fr>
2017-01-27 10:11:54 +0100

    LL

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>  /* Affichage */
+#include <stdlib.h> 
+
+/* main contient le programme */
+int main(int argc, char* argv[])
+{
+    /* Entrée du programme */
+
+    /* Initialisation des variables */
+	int a = 2;
+    int b = 3;
+    int c = a + b;
+
+    /* Affichage de c le résultat */
+    printf("%d", c);
+
+    /* Sortie du Porgramme */
+	return EXIT_SUCCESS;
+}
